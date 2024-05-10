0c99cc5b5d385b120d730c85927a0d75a6b12645
Nisanth Chunduru <nisanth074@gmail.com>
2024-05-10 15:31:04 +0530

    Modify generate_capthas.go to accept the number of captchas to generate and the directory to save captchas in as command line arguments

--- generate_captchas.go
+++ generate_captchas.go
@@ -4,21 +4,38 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
 
 	"github.com/dchest/captcha"
 )
 
 func main() {
-	generateCaptchas(10, "./data/captchas/")
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go run generate_captchas.go <count> <destination_directory>")
+		os.Exit(1)
+	}
+
+	count, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("Please provide the number of captchas you'd like to generate as the first argument")
+		os.Exit(1)
+	}
+
+	destinationDirectory := filepath.Clean(os.Args[2])
+
+	generateCaptchas(count, destinationDirectory)
 }
 
 func generateCaptchas(count int, destinationDirectory string) {
-	// Ensure the destination directory exists
 	if err := os.MkdirAll(destinationDirectory, 0755); err != nil {
 		panic(err)
 	}
 
-	for i := 0; i < count; i++ {
+	startIndex := findStartIndex(destinationDirectory)
+
+	for i := startIndex; i < count; i++ {
 		captchaDigits := captcha.RandomDigits(6)
 		captchaDummyId := "dummyId"
 		captchaWidth := 120
@@ -41,4 +58,26 @@ func generateCaptchas(count int, destinationDirectory string) {
 			panic(err)
 		}
 	}
+}
+
+func findStartIndex(destinationDirectory string) int {
+	files, err := os.ReadDir(destinationDirectory)
+	if err != nil {
+		panic(err)
+	}
+
+	maxIndex := 0
+	for _, file := range files {
+		name := file.Name()
+		if strings.HasSuffix(name, ".png") {
+			parts := strings.Split(name, "_")
+			if len(parts) > 0 {
+				index, err := strconv.Atoi(parts[0])
+				if err == nil && index > maxIndex {
+					maxIndex = index
+				}
+			}
+		}
+	}
+	return maxIndex
 }
